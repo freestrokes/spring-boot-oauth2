ddd87e6c903498f45a6f6702e7fee41b663eee94
harry.khs <freestrokes.hk@gmail.com>
2023-02-14 10:24:21 +0900

    [spring-boot-oauth2] feat: init.

--- src/main/java/com/freestrokes/SpringBootOAuth2Application.java
+++ src/main/java/com/freestrokes/SpringBootOAuth2Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootOAuth2Application {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootOAuth2Application.class, args);
 	}
