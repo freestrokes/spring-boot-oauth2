14c1e20995b197604ca669e1449592a373dac685
freestrokes <freestrokes.hk@gmail.com>
2023-02-17 18:51:10 +0900

    [spring-boot-oauth2] feat: security 추가

---
+++ src/main/java/com/freestrokes/auth/security/JwtTokenFilter.java
@@ -0,0 +1,44 @@
+package com.freestrokes.auth.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// TODO: JwtTokenFilter
+// 로그인시 request에 담긴 토큰의 유효성을 확인하는 역할.
+
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void doFilterInternal(
+        HttpServletRequest httpServletRequest,
+        HttpServletResponse httpServletResponse,
+        FilterChain filterChain
+    ) throws IOException, ServletException {
+
+        // request header에서 jwt 토큰 가져오기
+        String token = jwtTokenProvider.resolveToken(httpServletRequest);
+
+        //  토큰 유효성 검사
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            // 토큰을 인증하고 결과를 인증객체(Authentication)에 저장
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+
+            // 토큰이 유효한 경우 인증된 상태를 유지하도록 SecurityContext에 Authentication 객체를 저장
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        // UsernamePasswordAuthenticationFilter 호출
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
---
+++ src/main/java/com/freestrokes/auth/security/JwtTokenProvider.java
@@ -0,0 +1,158 @@
+package com.freestrokes.auth.security;
+
+import com.freestrokes.properties.SecurityProperties;
+import com.freestrokes.constants.AuthConstants;
+import com.freestrokes.auth.dto.AuthDto;
+import com.freestrokes.auth.service.CustomUserDetailsService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtTokenProvider {
+
+    private final SecurityProperties securityProperties;
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    /**
+     * JWT 토큰 생성
+     *
+     * @param email
+     * @param role
+     * @return
+     */
+    public AuthDto.AuthTokenDto createJwtToken(String email, AuthConstants.Role role) {
+
+        Date now = new Date();
+
+        // Access Token 만료 시간
+        Date accessTokenExpiration = new Date(now.getTime() + securityProperties.getToken().getAccessTokenValidTime());
+
+        // TODO
+        // Refresh Token 만료 시간
+//        Date refreshTokenExpiration = new Date(now + securityProperties.getToken().getRefreshTokenValidTime());
+
+        // claims
+        Claims claims = Jwts.claims().setSubject(AuthConstants.ACCESS_TOKEN);
+//            .setSubject(email);
+        claims.put("email", email);
+        claims.put("role", role);
+
+        // secret key
+        Key secretKey = Keys.hmacShaKeyFor(securityProperties.getToken().getSecretKey().getBytes());
+
+        String accessToken = Jwts.builder()
+            .setClaims(claims)
+            .setIssuedAt(now)
+            .setExpiration(accessTokenExpiration)
+            .signWith(secretKey, SignatureAlgorithm.HS256)
+            // TODO: deprecated
+//            .signWith(SignatureAlgorithm.HS256, Base64.getEncoder().encodeToString(securityProperties.getToken().getSecretKey().getBytes()))
+            .compact();
+
+        return AuthDto.AuthTokenDto.builder()
+            .accessToken(accessToken)
+            .accessTokenExpiration(accessTokenExpiration)
+            // TODO
+//            .refreshToken(refreshToken)
+//            .refreshTokenExpiration(refreshTokenExpiration)
+            .build();
+
+    }
+
+    /**
+     * JWT 토큰 인증 조회
+     *
+     * @param token
+     * @return
+     */
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(getUsername(token));
+
+        return new UsernamePasswordAuthenticationToken(
+            userDetails,
+            "",
+            userDetails.getAuthorities()
+        );
+    }
+
+    /**
+     * JWT 토큰 username(email) 조회
+     *
+     * ex) jwtTokenProvider.getUserEmail(accessToken);
+     *
+     * @param token
+     * @return
+     */
+    public String getUsername(String token) {
+        // TODO: Jwts.parser() Deprecated
+//        Jwts.parser()
+//            .setSigningKey(securityProperties.getToken().getSecretKey().getBytes())
+//            .parseClaimsJws(token).getBody().getSubject()
+
+        return Jwts.parserBuilder()
+            .setSigningKey(securityProperties.getToken().getSecretKey().getBytes())
+            .build()
+            .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    /**
+     * JWT 토큰 가져오기 (요청 헤더)
+     *
+     * @param httpServletRequest
+     * @return
+     */
+    public String resolveToken(HttpServletRequest httpServletRequest) {
+        // request header에서 토큰 가져오기
+        String bearerToken = httpServletRequest.getHeader(AuthConstants.AUTHORIZATION_HEADER);
+
+        if (bearerToken != null && bearerToken.startsWith(AuthConstants.BEARER_TYPE)) {
+            // TODO
+//            String[] tokenArray = bearerToken.split("Bearer");
+//            if (tokenArray.length > 0) {
+//                  return tokenArray[tokenArray.length - 1];
+//            }
+
+            return bearerToken.substring(AuthConstants.BEARER_TYPE.length()).trim();
+        }
+
+        return null;
+    }
+
+    /**
+     * JWT 토큰 만료 확인 (만료 여부 true/false 반환)
+     *
+     * @param token
+     * @return
+     */
+    public boolean validateToken(String token) {
+        try {
+            Date now = new Date();
+
+            // claims 조회
+            Claims claims = Jwts.parserBuilder()
+                .setSigningKey(securityProperties.getToken().getSecretKey().getBytes())
+                .build()
+                .parseClaimsJws(token).getBody();
+
+            return !claims.getExpiration().before(now);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
